c276529601123614d96a2f36b1dba1697057c759
Thangph13623 <halloeveryone2002@gmail.com>
2022-12-07 15:16:13 +0700

    update chuc nang

--- delete_user.php
+++ delete_user.php
@@ -4,11 +4,10 @@
 if(isset($_REQUEST['email']) and $_REQUEST['email']!=""){
 $id=$_GET['email'];
 $sql = "DELETE FROM Customer WHERE email='$id';";
-// $query_customer = mysqli_query($conn,$sql);
 if (mysqli_query($conn, $sql)) {
-    echo "Record deleted successfully";
+    echo "Xóa Thành Công";
   } else {
-    echo "Error deleting record: " . mysqli_error($conn);
+    echo "Lỗi Không Thành Công: " . mysqli_error($conn);
   }
 }
 ?>
